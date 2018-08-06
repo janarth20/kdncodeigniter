fc71bd22971e085ca80c36a0d02f46d81b21f0e1
janarth20 <jana_sharma@yahoo.com>
2018-08-06 16:37:09 +0800

    logout

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -3,8 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		//check if the user logged in all function in this file. this to avoid to have the below function in all function in this file
+		if (!$this->ion_auth->logged_in())
+		{
+			redirect('auth/login');
+		}
+	}
+
 	public function index()
 	{
+		
 		//declare empty array
 		$data=array();
 
